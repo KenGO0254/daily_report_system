02a4454345b4dbf5107668ee940793f8cdaa8dcc
KenGO <kengohsyukatsu@gmail.com>
2022-08-16 08:32:53 +0900

    日報テーブルに「いいね数」を追加

--- src/main/java/actions/views/ReportConverter.java
+++ src/main/java/actions/views/ReportConverter.java
@@ -24,7 +24,8 @@ public class ReportConverter {
 				rv.getTitle(),
 				rv.getContent(),
 				rv.getCreatedAt(),
-				rv.getUpdatedAt());
+				rv.getUpdatedAt(),
+				rv.getLikeCount());
 	}
 
 	/**
@@ -44,7 +45,8 @@ public class ReportConverter {
 				r.getTitle(),
 				r.getContent(),
 				r.getCreatedAt(),
-				r.getUpdatedAt());
+				r.getUpdatedAt(),
+				r.getLikeCount());
 	}
 
 	/**
@@ -75,5 +77,6 @@ public class ReportConverter {
 		r.setContent(rv.getContent());
 		r.setCreatedAt(rv.getCreatedAt());
 		r.setUpdatedAt(rv.getUpdatedAt());
+		r.setLikeCount(rv.getLikeCount());
 	}
 }
